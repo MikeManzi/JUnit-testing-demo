7e8da1a64cc816dde25a180a9aef6d4a9347247b
MikeManzi <manzimike378@gmail.com>
2021-11-26 11:08:32 +0200

    chore: constructor

--- src/main/java/com/example/testing_work/model/Student.java
+++ src/main/java/com/example/testing_work/model/Student.java
@@ -12,6 +12,16 @@ public class Student {
     private String lastName;
     private String email;
 
+    public Student() {
+    }
+
+    public Student(int id, String firstName, String lastName, String email) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+    }
+
     public int getId() {
         return id;
     }
